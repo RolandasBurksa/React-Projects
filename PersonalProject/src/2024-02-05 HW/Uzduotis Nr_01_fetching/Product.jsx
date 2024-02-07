757fb6f9488163f58be245f0523165c00463e912
RolandasBurksa <rburksa@gmail.com>
2024-02-07 13:10:01 +0200

    Added data fetching

--- PersonalProject/src/2024-02-05 HW/Uzduotis Nr_01_fetching/Product.jsx
+++ PersonalProject/src/2024-02-05 HW/Uzduotis Nr_01_fetching/Product.jsx
@@ -1,8 +1,12 @@
-export const Product = ({ title }) => {
+export const Product = ({ thumbnail, title, description, price, stock }) => {
   return (
     <>
-      <div>
-        <h1>{title}</h1>
+      <div style={{ marginBottom: '50px' }}>
+        <img src={thumbnail} alt="some text" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>{price}</p>
+        <p>{stock}</p>
       </div>
       Product
     </>
--- PersonalProject/src/2024-02-05 HW/Uzduotis Nr_01_fetching/ProductListDataFetching.jsx
+++ PersonalProject/src/2024-02-05 HW/Uzduotis Nr_01_fetching/ProductListDataFetching.jsx
@@ -4,18 +4,35 @@ import { Product } from './Product';
 
 export const ProductListDataFetching = () => {
   const [productArray, setProductArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('https://dummyjson.com/products').then((response) => setProductArray(response.data.products));
+    axios
+      .get('https://dummyjson.com/products')
+      .then((response) => {
+        setProductArray(response.data.products);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   console.log('products', productArray);
+
+  if (isLoading) {
+    return <div>Laukiame uzkrovimo pabaigos</div>;
+  }
 
   return (
     <>
       <h1>Produkt listo itemai</h1>
       {productArray.map((product) => (
-        <Product key={product.id} title={product.title} />
+        <Product
+          key={product.id}
+          thumbnail={product.thumbnail}
+          title={product.title}
+          price={`price - ${product.price}`}
+          stock={`qantity - ${product.stock}`}
+        />
       ))}
     </>
   );
